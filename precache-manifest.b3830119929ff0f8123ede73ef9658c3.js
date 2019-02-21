a733e552e73364df1ef3ff7721b335ec74b1e335
David <lemmih@gmail.com>
2019-02-21 18:01:41 +0100

    Updates

---
+++ precache-manifest.b3830119929ff0f8123ede73ef9658c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "78b70e4ae5c862d7d7c1",
+    "url": "/reanimate/static/js/runtime~main.78b70e4a.js"
+  },
+  {
+    "revision": "70f4484c7843ce2202b3",
+    "url": "/reanimate/static/js/main.70f4484c.chunk.js"
+  },
+  {
+    "revision": "45475a7b914615f56919",
+    "url": "/reanimate/static/js/2.45475a7b.chunk.js"
+  },
+  {
+    "revision": "70f4484c7843ce2202b3",
+    "url": "/reanimate/static/css/main.6d4cd12a.chunk.css"
+  },
+  {
+    "revision": "80d44fbcdca93856a4d60910a91ada82",
+    "url": "/reanimate/index.html"
+  }
+];
